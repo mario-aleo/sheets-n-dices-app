5980691091ee830f5e7b92566123991a4e16dd0b
Mario Aleo <mario.aleo@golsat.com.br>
2019-05-16 17:37:07 -0300

    feat(app): create signin overlay and auth management

--- src/sheets-n-dices.js
+++ src/sheets-n-dices.js
@@ -3,6 +3,11 @@ import { installRouter, installOfflineWatcher } from 'pwa-helpers';
 import '@polymer/app-layout/app-header/app-header';
 import '@polymer/app-layout/app-toolbar/app-toolbar';
 import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/paper-button/paper-button';
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/iron-icon/iron-icon';
+import '@polymer/iron-icons/iron-icons';
+import { signIn, signOut, authChanged } from './snd-firebase/snd-firebase';
 import './snd-home/snd-home';
 
 class SheetsNDices extends LitElement {
@@ -16,7 +21,14 @@ class SheetsNDices extends LitElement {
       offline: {
         type: Boolean,
         reflect: true,
-        attribute: false,
+      },
+      authorized: {
+        type: Boolean,
+        reflect: true,
+      },
+      unresolved: {
+        type: Boolean,
+        reflect: true,
       },
     };
   }
@@ -33,53 +45,133 @@ class SheetsNDices extends LitElement {
             'app-body';
           min-height: 100vh;
           width: 100vw;
+          contain: strict;
+
+          --primary-color: #673ab7;
+          --dark-primary-color: #37006a;
+          --light-primary-color: #9b4dcb;
+          --on-primary-color: #fff;
+
+          --secondary-color: #03a9f4;
+          --dark-secondary-color: #0094cc;
+          --light-secondary-color: #82f7ff;
+          --on-secondary-color: #000;
         }
 
         #app-header {
           grid-area: app-header;
           position: fixed;
           width: 100vw;
+          color: var(--on-primary-color);
+          background-color: var(--primary-color);
+        }
+
+        #offline {
+          opacity: 0;
+          visibility: hidden;
+          transition: opacity ease-in-out 0.33s;
+          will-change: opacity, transition;
+          contain: content;
+        }
+        :host([offline]) #offline {
+          opacity: 1;
+          visibility: visible;
+        }
+
+        #signout {
+          opacity: 0;
+          visibility: hidden;
+          transition: opacity ease-in-out 0.33s;
+          will-change: opacity, transition;
+          contain: content;
+        }
+        :host([authorized]) #signout {
+          opacity: 1;
+          visibility: visible;
         }
 
         #app-body {
           grid-area: app-body;
         }
 
-        app-header {
-          background-color: red;
+        #app-login-overlay {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          position: fixed;
+          top: 64px;
+          left: 0;
+          width: 100vw;
+          height: calc(100vh - 64px);
+          color: var(--on-primary-color);
+          background-color: var(--primary-color);
+          transition: transform ease-in-out 0.33s;
+          will-change: transform, transition;
+          contain: content;
+        }
+        :host([authorized]) #app-login-overlay {
+          transform: translateY(100%);
+        }
+
+        #signin {
+          opacity: 1;
+          visibility: visible;
+          transition: opacity ease-in-out 0.33s;
+          will-change: opacity, transition;
+          contain: content;
+        }
+        :host([authorized]) #signin,
+        :host([unresolved]) #signin {
+          opacity: 0;
+          visibility: hidden;
         }
       `,
     ];
   }
 
+  /* Lifecycle */
   constructor() {
     super();
 
-    installRouter((location, event) => {
-      if (event && event.type === 'click') window.scrollTo(0, 0);
-      this.route = location.pathname;
-    });
+    this.route = '';
+    this.offline = false;
+    this.authorized = false;
+    this.unresolved = true;
 
-    installOfflineWatcher(offline => {
-      this.offline = offline;
-    });
+    authChanged(this._onAuthChanged.bind(this));
+    installRouter(this._onRouteChanged.bind(this));
+    installOfflineWatcher(this._onOfflineChanged.bind(this));
   }
 
+  firstUpdated() {}
+  /* */
+
+  /* Render */
   render() {
     return html`
       <app-header id="app-header" fixed effects="waterfall">
         <app-toolbar>
           <div main-title>Sheets 'n Dices</div>
+          <iron-icon id="offline" icon="cloud-off"></iron-icon>
+          <paper-icon-button
+            id="signout"
+            icon="power-settings-new"
+            @click="${signOut}"
+          ></paper-icon-button>
         </app-toolbar>
       </app-header>
 
       <section id="app-body">
-        ${this.renderRoute()}
+        ${this._renderRoute()}
+      </section>
+
+      <section id="app-login-overlay">
+        <paper-button id="signin" raised @click="${signIn}">Sign In</paper-button>
       </section>
     `;
   }
 
-  renderRoute() {
+  _renderRoute() {
     switch (this.route) {
       case '/create':
         return html`
@@ -92,6 +184,26 @@ class SheetsNDices extends LitElement {
         `;
     }
   }
+  /* */
+
+  /* Private */
+  _onOfflineChanged(offline) {
+    this.offline = offline;
+  }
+
+  _onRouteChanged(location, event) {
+    if (event && event.type === 'click') window.scrollTo(0, 0);
+    this.route = location.pathname;
+  }
+
+  _onAuthChanged(auth) {
+    this.unresolved = false;
+    this.authorized = !!auth;
+  }
+  /* */
+
+  /* Public */
+  /* */
 }
 
 customElements.define('sheets-n-dices', SheetsNDices);
